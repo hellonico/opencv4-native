c2de56e46c997e2c9d59d94ee95243bfeae19685
Nicolas Modrzyk <hellonico@gmail.com>
2025-05-12 12:45:12 +0900

    removing old folder

--- clojars/deploy.sh
+++
@@ -1,3 +0,0 @@
-# lein deploy clojars opencv/opencv 4.0.0-0 opencv-4.0.0-0.jar opencv-4.0.0-0.pom 
-
-lein deploy clojars opencv/opencv-native 4.0.0-1 opencv-native-4.0.0-1.jar	opencv-native-4.0.0-1.pom.xml

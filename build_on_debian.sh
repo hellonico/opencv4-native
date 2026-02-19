8607f9f79c5f7b5e066b85d9a2b7698b1da61f79
Nicolas Modrzyk <hellonico@gmail.com>
2026-02-19 09:15:33 +0100

    minimal debian build

--- build_on_debian.sh
+++ build_on_debian.sh
@@ -18,7 +18,7 @@ else
 fi
 echo "Using downloader: $DL_CMD (binary)"
 
-echo "Using downloader: $DL_CMD (binary)"
+
 
 # Version comparison function (returns 0=equal, 1=ver1>ver2, 2=ver1<ver2)
 vercomp () {
@@ -48,20 +48,7 @@ else
     echo "WARNING: python3 not found. Build may fail."
 fi
 
-# Version comparison function (returns 0=equal, 1=ver1>ver2, 2=ver1<ver2)
-vercomp () {
-    if [[ $1 == $2 ]]; then return 0; fi
-    local IFS=.
-    local i ver1=($1) ver2=($2)
-    # fill empty fields in ver1 with zeros
-    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++)); do ver1[i]=0; done
-    for ((i=0; i<${#ver1[@]}; i++)); do
-        if [[ -z ${ver2[i]} ]]; then ver2[i]=0; fi
-        if ((10#${ver1[i]} > 10#${ver2[i]})); then return 1; fi
-        if ((10#${ver1[i]} < 10#${ver2[i]})); then return 2; fi
-    done
-    return 0
-}
+
 
 # --- 1. CMake Check ---
 NEED_CMAKE=0
